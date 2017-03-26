441b57c11f6f59a0c7bf57e7fb899dfce91d535e
Drew Miley <drewmiley92@gmail.com>
2017-03-26 11:31:16 +0100

    AboutScoringProject Complete

--- python3/koans/about_scoring_project.py
+++ python3/koans/about_scoring_project.py
@@ -34,7 +34,32 @@ from runner.koan import *
 
 def score(dice):
     # You need to write this method
-    pass
+    ones = dice.count(1)
+    twos = dice.count(2)
+    threes = dice.count(3)
+    fours = dice.count(4)
+    fives = dice.count(5)
+    sixes = dice.count(6)
+
+    score = 0
+
+    if ones >= 3:
+        score += 1000 + (ones - 3) * 100
+    else:
+        score += ones * 100
+    if twos >= 3:
+        score += 200
+    if threes >= 3:
+        score += 300
+    if fours >= 3:
+        score += 400
+    if fives >= 3:
+        score += 500 + (fives - 3) * 50
+    else:
+        score += fives * 50
+    if sixes >=3:
+        score += 600
+    return score
 
 class AboutScoringProject(Koan):
     def test_score_of_an_empty_list_is_zero(self):
